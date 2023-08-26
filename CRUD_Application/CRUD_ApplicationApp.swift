6180b110202c22dfedbcebc18ed4aabb95b9e5ca
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-08-26 22:54:01 +0600

    Initial Commit

---
+++ CRUD_Application/CRUD_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_ApplicationApp.swift
+//  CRUD_Application
+//
+//  Created by A.f. Adib on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
