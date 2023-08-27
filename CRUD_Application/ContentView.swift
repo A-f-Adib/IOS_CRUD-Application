90061427a94c63c5b9d7b412e1dda971993404b1
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-08-27 06:20:21 +0600

    commit-1

--- CRUD_Application/ContentView.swift
+++ CRUD_Application/ContentView.swift
@@ -8,10 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fruits : [String] = ["Apples", "oranges", "bananas", "lemons"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            List{
+                
+                Section {
+                    ForEach(fruits, id: \.self) { Index in
+                        Text(Index.capitalized)
+                            .padding(.vertical)
+                    }
+                      
+                      .onDelete(perform: deelete)
+                      .onMove(perform: move)
+                      .listRowBackground(Color.mint)
+                      .font(.title3)
+                    
+                }
+                
+                
+            header: {
+                HStack{
+                    Text("Fruits")
+                    Image(systemName: "smiley")
+                }
+                    
+                    .foregroundColor(Color.pink)
+                        .font(.largeTitle)
+                }   .accentColor(Color.mint)
+                
+                
+                Section {
+                    ForEach(fruits, id: \.self) { Index in
+                        Text(Index.capitalized)
+                    } .listRowBackground(Color.cyan)
+                    
+                }
+            header: {
+                HStack{
+                    Text("Vegetables")
+                        .foregroundColor(Color.purple)
+                    Image(systemName: "")
+                }
+                    
+                  
+                        .font(.title)
+                }
+                
+            }
+            
+            
+            
+//            .listStyle(DefaultListStyle())
+            .navigationTitle(Text("Grocery items"))
+            
+            .navigationBarItems(leading: EditButton(), trailing:Button("Add",
+                action: {
+                        fruits.append("coconut")
+            }))
+            
+        }
+        
+        }
+    func deelete( indexSet : IndexSet){
+        fruits.remove(atOffsets: indexSet)
     }
+    
+    func move(indices : IndexSet, newPlace : Int ){
+        fruits.move(fromOffsets: indices , toOffset: newPlace)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
